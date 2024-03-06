a008aaa0a71e8da7a62eb2306ec22088f8b11603
Md Manowar Hashmi <42206806+gemsstone@users.noreply.github.com>
2024-03-06 23:31:43 +0530

    Delete assets directory

--- assets/js/custom.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function() {
-    // Hide the loader
-    document.querySelector('.loaderWrapper').style.display = 'none';
-    // Show the content
-    document.querySelector('.content').style.display = 'block';
-  }, 3000);
